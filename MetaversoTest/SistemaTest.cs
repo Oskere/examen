379d9d7b579f50372844cb30887704d4b26ff64b
Oskere <91115094+Oskere@users.noreply.github.com>
2022-01-26 11:36:50 +0100

    Update SistemaTest.cs

--- MetaversoTest/SistemaTest.cs
+++ MetaversoTest/SistemaTest.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 using Metaverso;
-namespace MetaversoTest
+namespace MetaversoTests
 {
     public class SistemaTest
     {   
@@ -11,21 +11,23 @@ namespace MetaversoTest
         [InlineData(15,"Metaverso")]
         [InlineData(5,"Verso")]
         [InlineData(3,"Meta")]
-        [InlineData(2,"2")]
+        [InlineData(8,"ocho")]
         public void Testmetaverso(int numero,string expected)
         {
             
             
-            String resultado = sis.metaverso(numero);
+            String resultado = sis.metaverso(num);
 
             Assert.Equal(resultado,expected);
         }
         [Theory]
         [InlineData(new int[0],"")]
-        [InlineData(new int[]{15,1},"Metaverso1")]
-        public void Testmetaverso2(int[] numero, string expected){
+        [InlineData(new int[]{15,1},"Metaverso")]
+        
+        
+        public void MetaversoTest2(int[] numero, string expected){
 
-            String resultado = sis.metaverso(numero);
+            String resultado = sis.metaverso(num);
             Assert.Equal(resultado,expected);
 
         }
