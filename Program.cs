3e70e2c953c14bc015d62e648bc1ac28883cbcc3
Oskere <91115094+Oskere@users.noreply.github.com>
2022-01-26 11:45:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Metaverso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sistema = new Sistema();
+            Console.WriteLine("Hola, bienvenido al programa del Metaverso");
+        }
+    }
+}
---
+++ Sistema.cs
@@ -0,0 +1,25 @@
+namespace Metaverso
+{
+    public class Sistema
+    {
+        
+        public string metaverso(int num){
+            if (num%5==0 ){
+                if (num % 3==0){
+                    return "Metaverso";
+                }
+                return "Verso";
+            } else if (num%3==0){
+                return "Meta";
+            } 
+            return num.ToString();
+        }
+        public string metaverso(int[] nums){
+           string resultado="";
+           foreach(int num in nums){
+               resultado+=metaverso(num);
+           } 
+           return resultado;
+        }
+    }
+}
